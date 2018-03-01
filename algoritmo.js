9fc1b4340a517e50c012a44fc54e11154d3a8392
Dani Vázquez <daniel.vazquez@s4g.es>
2018-03-01 19:26:25 +0100

    algoritmo v0.1

---
+++ algoritmo.js
@@ -0,0 +1,29 @@
+var cars = [];
+var riders = [];
+
+//Inicialmente son todos
+var emptyCars = [];
+var emptyRiders = [];
+
+//
+var score = 0;
+var actualScore = 0;
+
+
+var totalTime = 10;
+var actualTime =0;
+
+var move = function(totalTime,cars,riders,score,actualScore,actualTime){
+  
+  if(actualTime <=totalTime){
+    
+    cars.forEach(function(car){
+      if(car.isBus)
+    })    
+    
+    
+    //Recursivo
+  } else {
+    return actualScore;
+  }
+}
