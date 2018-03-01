f8e1930eb8ebeb1818b7d22c827c75ad6743634c
Víctor <vmandrespascual@gmail.com>
2018-03-01 20:22:07 +0100

    Reader Game && riders

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-require('./models.js');
-require('./algoritmo.js');
+var Models = require('./models.js');
+var Algoritmo = require('./algoritmo.js');
 
 
 var fs = require('fs');
-
-
 
 var nameFile = null;
 nameFile = process.argv[3];
@@ -12,36 +10,29 @@ nameFile = process.argv[3];
 if(nameFile != null){
 	nameFile = process.env.NFILE;
 }else{
-	nameFile = 'a_example.in';
+	nameFile = 'a_example.in'; //Name File
 }
 //const nameFile = require('nameFile');
-
+console.log('Inicio');
 var archivo = fs.readFileSync(nameFile, 'utf-8');
-var matrix = [];
+var riders = [];
 var conf = [];
+
 var matrixValue = [];
 
 	//console.log(archivo.slice(0, c+2));
 	matrix = archivo.split("\n");
+	conf = matrix[0].split(" ");
 
-	matrix.forEach(function(element, index, array){
-		if(index == 0){
-			conf = element.split(" ");
-		}else{
-			if(element != ''){
-				tmpValue = [];
-				row = element.split(" ");			
-				for(i = 0; i< row[0].length; i++){
-					//console.log(row[0][i]);
-					tmpValue.push(row[0][i]);
-				}				
-				matrixValue[index-1] = tmpValue;
-			}
-		}
-	});
-		
-	console.log(conf);
-	//console.log(matrixValue);
+	var Game = new Models.Game(conf[0],conf[1],conf[2],conf[3],conf[4],conf[5]);
 
+	var linea = 1;
+	for(i = 0; i < Game.rides; i++){
+		var maEp = matrix[linea].split(' ');
+		console.log(maEp);
+		riders[linea] = new Models.Ride(new Models.Location(maEp[0],maEp[1]),new Models.Location(maEp[2],maEp[3]),maEp[4],maEp[5], linea);
+		linea++;
+	}
 
-	var Game = new Game(conf[0],conf[1],conf[2],conf[3],conf[4],conf[6]);
+	console.log(Game);
+	console.log(riders);
